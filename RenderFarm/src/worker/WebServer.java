417040d34f9e3ff96643898cb5eaca88c48c986c
dharuq <dharuq@gmail.com>
2017-04-02 13:03:53 +0100

    Project

---
+++ RenderFarm/src/worker/WebServer.java
@@ -0,0 +1,62 @@
+package worker;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class WebServer {
+
+	// http://<load-balancer-DNS-name>/r.html?f=<model-filename>&sc=<scene-columns>&sr=<scene-rows>&wc=<window-columns>&wr=<window-rows>&coff=<column-offset>&roff=<row-offset>
+
+	public static void main(String[] args) throws Exception {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		server.createContext("/r.html", new MyHandler());
+		// server.setExecutor(null); // creates a default executor
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.start();
+		
+		
+	}
+
+	static class MyHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			String response = "This was the query:" + t.getRequestURI().getQuery() + "##";
+			
+			Map<String, String> params = queryToMap(t.getRequestURI().getQuery()); 
+			String concat1 = new String();
+			
+			for (String key : params.keySet()) {
+			    concat1.concat(key + " " + params.get(key) + "\n");
+			}
+			
+			t.sendResponseHeaders(200, concat1.length());
+			OutputStream os = t.getResponseBody();
+			os.write(concat1.getBytes());
+			os.close();
+		}
+	}
+	
+	static Map<String, String> queryToMap(String query){
+	    Map<String, String> result = new HashMap<String, String>();
+	    for (String param : query.split("&")) {
+	    	
+	        String pair[] = param.split("=");
+	        if (pair.length>1) {
+	            result.put(pair[0], pair[1]);
+			System.out.println(pair[0] + " : " + pair[1]);
+	        }else{
+	            result.put(pair[0], "");
+	        }
+	    }
+	    return result;
+	}
+
+}
