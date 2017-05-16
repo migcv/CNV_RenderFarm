cb3a3574250c7f31be1b48b7d7d490547230fc50
EC2 Default User <ec2-user@ip-172-31-2-239.us-west-2.compute.internal>
2017-05-16 01:27:03 +0000

    Load and store counters are set to zero after every write to file

--- BIT/samples/MyTool.java
+++ BIT/samples/MyTool.java
@@ -22,14 +22,14 @@ import BIT.highBIT.*;
 
 public class MyTool {
     /*
-     * 	ICount
+     *  ICount
      */
     private static ConcurrentHashMap<Long, Long> i_count = new ConcurrentHashMap<Long, Long>();
     private static ConcurrentHashMap<Long, Long> b_count = new ConcurrentHashMap<Long, Long>();
     /*
-     * 	StatisticTool -load_store
+     *  StatisticTool -load_store
      */
-	private static ConcurrentHashMap<Long, Long> loadcount = new ConcurrentHashMap<Long, Long>();
+    private static ConcurrentHashMap<Long, Long> loadcount = new ConcurrentHashMap<Long, Long>();
     private static ConcurrentHashMap<Long, Long> storecount = new ConcurrentHashMap<Long, Long>();
     private static ConcurrentHashMap<Long, Long> fieldloadcount = new ConcurrentHashMap<Long, Long>();
     private static ConcurrentHashMap<Long, Long> fieldstorecount = new ConcurrentHashMap<Long, Long>();
@@ -43,17 +43,17 @@ public class MyTool {
         for (int i = 0; i < infilenames.length; i++) {
             String filename = infilenames[i];
             if (filename.endsWith(".class")) {
-				// create class info object
-            	String in_filename = file_in.getAbsolutePath() + System.getProperty("file.separator") + filename;
-				String out_filename = file_out.getAbsolutePath() + System.getProperty("file.separator") + filename;
-				ClassInfo ci = new ClassInfo(in_filename);
-				
+                // create class info object
+                String in_filename = file_in.getAbsolutePath() + System.getProperty("file.separator") + filename;
+                String out_filename = file_out.getAbsolutePath() + System.getProperty("file.separator") + filename;
+                ClassInfo ci = new ClassInfo(in_filename);
+                
                 // loop through all the routines
                 // see java.util.Enumeration for more information on Enumeration class
                 for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
                     Routine routine = (Routine) e.nextElement();
                     /*
-                     * 	ICount
+                     *  ICount
                      */
                     for (Enumeration b = routine.getBasicBlocks().elements(); b.hasMoreElements(); ) {
                         BasicBlock bb = (BasicBlock) b.nextElement();
@@ -70,35 +70,35 @@ public class MyTool {
 
                         }
                     }
-					/*
-                     * 	StatisticTool -load_store
+                    /*
+                     *  StatisticTool -load_store
                      */
-					/*
-					for (Enumeration instrs = (routine.getInstructionArray()).elements(); instrs.hasMoreElements(); ) {
-						Instruction instr = (Instruction) instrs.nextElement();
-						int opcode=instr.getOpcode();
-						if (opcode == InstructionTable.getfield)
-							instr.addBefore("MyTool", "LSFieldCount", new Integer(0));
-						else if (opcode == InstructionTable.putfield)
-							instr.addBefore("MyTool", "LSFieldCount", new Integer(1));
-						/*else {
-							short instr_type = InstructionTable.InstructionTypeTable[opcode];
-							if (instr_type == InstructionTable.LOAD_INSTRUCTION) {
-								instr.addBefore("MyTool", "LSCount", new Integer(0));
-							}
-							else if (instr_type == InstructionTable.STORE_INSTRUCTION) {
-								instr.addBefore("MyTool", "LSCount", new Integer(1));
-							}
-						}*/
-					//}
+                    /*
+                    for (Enumeration instrs = (routine.getInstructionArray()).elements(); instrs.hasMoreElements(); ) {
+                        Instruction instr = (Instruction) instrs.nextElement();
+                        int opcode=instr.getOpcode();
+                        if (opcode == InstructionTable.getfield)
+                            instr.addBefore("MyTool", "LSFieldCount", new Integer(0));
+                        else if (opcode == InstructionTable.putfield)
+                            instr.addBefore("MyTool", "LSFieldCount", new Integer(1));
+                        /*else {
+                            short instr_type = InstructionTable.InstructionTypeTable[opcode];
+                            if (instr_type == InstructionTable.LOAD_INSTRUCTION) {
+                                instr.addBefore("MyTool", "LSCount", new Integer(0));
+                            }
+                            else if (instr_type == InstructionTable.STORE_INSTRUCTION) {
+                                instr.addBefore("MyTool", "LSCount", new Integer(1));
+                            }
+                        }*/
+                    //}
 
                 }   
                 /*
-                 * 	ICount
+                 *  ICount
                  */
                 ci.addAfter("MyTool", "printICount", ci.getClassName());
                 /*
-                 * 	StatisticTool -load_store
+                 *  StatisticTool -load_store
                  */
                 ci.addAfter("MyTool", "printLoadStore", ci.getClassName());
                 ci.write(out_filename);
@@ -106,94 +106,94 @@ public class MyTool {
         }
     }    
     /*
-     * 	ICount methods
+     *  ICount methods
      */
     public static synchronized void printICount(String foo) {
-    	try {
-    		FileWriter log = new FileWriter("log/" + getThreadId() +".txt", true);
-			log.write(foo + " - ICOUNT: " + "\n"
-					+ "Number of basic blocks:      " + b_count.get(getThreadId()) + "\n" 
-					+ "Number of instructions:      " + i_count.get(getThreadId()) + "\n");
-			float instr_per_bb = (float) i_count.get(getThreadId()) / (float) b_count.get(getThreadId());
-			log.write("Average number of instructions per basic block: " + instr_per_bb + "\n");
-			log.close();
-			i_count.put(getThreadId(), new Long(0));
-    		b_count.put(getThreadId(), new Long(0));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            FileWriter log = new FileWriter("log/" + getThreadId() +".txt", true);
+            log.write(foo + " - ICOUNT: " + "\n"
+                    + "Number of basic blocks:      " + b_count.get(getThreadId()) + "\n" 
+                    + "Number of instructions:      " + i_count.get(getThreadId()) + "\n");
+            float instr_per_bb = (float) i_count.get(getThreadId()) / (float) b_count.get(getThreadId());
+            log.write("Average number of instructions per basic block: " + instr_per_bb + "\n");
+            log.close();
+            i_count.put(getThreadId(), new Long(0));
+            b_count.put(getThreadId(), new Long(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public static synchronized void count(int incr) {
-    	if(i_count.get(getThreadId()) != null && b_count.get(getThreadId()) != null) {
-    		i_count.put(getThreadId(), i_count.get(getThreadId()) + incr);
-    		b_count.put(getThreadId(), b_count.get(getThreadId()) + 1);
-    	} else {
-    		i_count.put(getThreadId(), new Long(incr));
-    		b_count.put(getThreadId(), new Long(1));
-    	}
+        if(i_count.get(getThreadId()) != null && b_count.get(getThreadId()) != null) {
+            i_count.put(getThreadId(), i_count.get(getThreadId()) + incr);
+            b_count.put(getThreadId(), b_count.get(getThreadId()) + 1);
+        } else {
+            i_count.put(getThreadId(), new Long(incr));
+            b_count.put(getThreadId(), new Long(1));
+        }
     }
     /*
-     * 	StatisticTool -load_store methods
+     *  StatisticTool -load_store methods
      */
     public static synchronized void printLoadStore(String foo) {
-    	try {
-			FileWriter log = new FileWriter("log/" + getThreadId() +".txt", true);
-			log.write(foo + " - LOAD_STORE: " + "\n"
-					+ "Field load:::    " + (fieldloadcount.get(getThreadId()) != null ? fieldloadcount.get(getThreadId()) : "0") + "\n"
-					+ "Field store:::  " + (fieldstorecount.get(getThreadId()) != null ? fieldstorecount.get(getThreadId()) : "0") + "\n"
-					+ "Regular load:  " + (loadcount.get(getThreadId()) != null ? loadcount.get(getThreadId()) : "0") + "\n"
-					+ "Regular store: " + (storecount.get(getThreadId()) != null ? storecount.get(getThreadId()) : "0") + "\n");
-		
-			log.close();
-    	} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            FileWriter log = new FileWriter("log/" + getThreadId() +".txt", true);
+            log.write(foo + " - LOAD_STORE: " + "\n"
+                    + "Field load:::    " + (fieldloadcount.get(getThreadId()) != null ? fieldloadcount.get(getThreadId()) : "0") + "\n"
+                    + "Field store:::  " + (fieldstorecount.get(getThreadId()) != null ? fieldstorecount.get(getThreadId()) : "0") + "\n"
+                    + "Regular load:  " + (loadcount.get(getThreadId()) != null ? loadcount.get(getThreadId()) : "0") + "\n"
+                    + "Regular store: " + (storecount.get(getThreadId()) != null ? storecount.get(getThreadId()) : "0") + "\n");
+            fieldloadcount.put(getThreadId(), new Long(0));
+            fieldstorecount.put(getThreadId(), new Long(0));
+            log.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static synchronized void LSFieldCount(int type) {
-		if (type == 0) {
-			//fieldloadcount++;
-			if(fieldloadcount.get(getThreadId()) != null) {
-				fieldloadcount.put(getThreadId(), fieldloadcount.get(getThreadId()) + 1);
-	    	} else {
-	    		fieldloadcount.put(getThreadId(), new Long(1));
-	    	}
-		}
-		else {
-			//fieldstorecount++;
-			if(fieldstorecount.get(getThreadId()) != null) {
-				fieldstorecount.put(getThreadId(), fieldstorecount.get(getThreadId()) + 1);
-	    	} else {
-	    		fieldstorecount.put(getThreadId(), new Long(1));
-	    	}
-		}
-	}
+        if (type == 0) {
+            //fieldloadcount++;
+            if(fieldloadcount.get(getThreadId()) != null) {
+                fieldloadcount.put(getThreadId(), fieldloadcount.get(getThreadId()) + 1);
+            } else {
+                fieldloadcount.put(getThreadId(), new Long(1));
+            }
+        }
+        else {
+            //fieldstorecount++;
+            if(fieldstorecount.get(getThreadId()) != null) {
+                fieldstorecount.put(getThreadId(), fieldstorecount.get(getThreadId()) + 1);
+            } else {
+                fieldstorecount.put(getThreadId(), new Long(1));
+            }
+        }
+    }
 
     public static synchronized void LSCount(int type) {
-		if (type == 0) {
-			//loadcount++;
-			if(loadcount.get(getThreadId()) != null) {
-				loadcount.put(getThreadId(), loadcount.get(getThreadId()) + 1);
-	    	} else {
-	    		loadcount.put(getThreadId(), new Long(1));
-	    	}
-		}
-		else {
-			//storecount++;
-			if(storecount.get(getThreadId()) != null) {
-				storecount.put(getThreadId(), storecount.get(getThreadId()) + 1);
-	    	} else {
-	    		storecount.put(getThreadId(), new Long(1));
-	    	}
-		}
-	}
+        if (type == 0) {
+            //loadcount++;
+            if(loadcount.get(getThreadId()) != null) {
+                loadcount.put(getThreadId(), loadcount.get(getThreadId()) + 1);
+            } else {
+                loadcount.put(getThreadId(), new Long(1));
+            }
+        }
+        else {
+            //storecount++;
+            if(storecount.get(getThreadId()) != null) {
+                storecount.put(getThreadId(), storecount.get(getThreadId()) + 1);
+            } else {
+                storecount.put(getThreadId(), new Long(1));
+            }
+        }
+    }
     /*
-     * 	Auxiliar methods
+     *  Auxiliar methods
      */
     public static synchronized Long getThreadId() {
-    	return Thread.currentThread().getId();
+        return Thread.currentThread().getId();
     }
 }
-
 
